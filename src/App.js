00806061c8a7d07912ebcbe918cb3ff20676870f
Mike <mike.m.herron@googlemail.com>
2022-04-21 10:44:38 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialFormState = {
+  name: '',
+  message: ''
 }
 
-export default App;
+function App() {
+  const [error, setError] = useState("")
+  const [entries, setEntries] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [form, setForm] = useState(initialFormState)
+
+  const onFormSubmit = (e) => {
+    e.preventDefault()
+    setError("")
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: form.name,
+        message: form.message
+      })
+    }
+
+    fetch(process.env.REACT_APP_API_URL + "/guestbook", options)
+      .then((res) => {
+        setIsLoading(false)
+        if (!res.ok) {
+          setError("Error adding entry")
+        } else {
+          setForm(initialFormState)
+          res.json().then((json) => {
+            setEntries([json.entry, ...entries])
+          })
+        }
+      })
+      .catch(() => {
+        setIsLoading(false)
+        setError("Error adding entry")
+      })
+  }
+
+  useEffect(() => {
+    setIsLoading(true)
+    fetch(process.env.REACT_APP_API_URL + "/guestbook")
+      .then((res) => {
+        setIsLoading(false)
+        if (!res.ok) {
+          setError("Error loading entries")
+        } else {
+          res.json().then((json) => {
+            setEntries(json.entries)
+          })
+        }
+      })
+      .catch(() => {
+        setIsLoading(false)
+        setError("Error loading entries")
+      })
+  }, [])
+
+  return (
+    <div className="Guestbook">
+      <header className="header">
+        <h1>Guestbook</h1>
+        <p>Welcome to the Guestbook, write a message below!</p>
+      </header>
+      <main>
+        {error && <span className="error">{error}</span>}
+        <form onSubmit={onFormSubmit}>
+          <label>
+            Name
+            <input value={form.name} onChange={(e) => setForm({...form, name: e.target.value})} required="required"/>
+          </label>
+          <label>
+            Message
+            <textarea value={form.message} onChange={(e) => setForm({...form, message: e.target.value})}  required="required" />
+          </label>
+          <input type="submit" value="Add Entry" />
+        </form>
+        {isLoading ? (
+          <p>Loading entries...</p>
+        ) : (
+          <ul>
+            {entries.map((entry, index) => (
+              <li key={index}>
+                {entry.message} -<strong>{entry.name}</strong>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
+    </div>
+  )
+}
+
+export default App
